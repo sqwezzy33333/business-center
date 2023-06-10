5791da4e423956cd046636a4041c39097eb36ab6
Sqwezzy33333 <sergey.kornev3@gmail.com>
2023-06-10 12:07:35 +0300

    feat: add header

---
+++ src/app/components/diagonal/diagonal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lending-diagonal',
+  templateUrl: './diagonal.component.html',
+  styleUrls: ['./diagonal.component.scss']
+})
+export class DiagonalComponent {
+
+}
